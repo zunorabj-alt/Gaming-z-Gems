2ace00ea7029cb753d40e85c096c1d85c476ac2a
zunorabj-alt <zunorabj@gmail.com>
2026-04-16 15:11:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,14 @@
 function showGame() {
-    const games = document.querySelector(".games");
-    const tarifs = document.getElementById("tarifs");
+    document.querySelector(".games").style.display = "none";
 
-    // smooth disappear
-    games.style.opacity = "0";
-    games.style.transform = "scale(0.95)";
-
-    setTimeout(() => {
-        games.style.display = "none";
-
-        tarifs.style.display = "block";
-        tarifs.style.opacity = "0";
-
-        setTimeout(() => {
-            tarifs.style.opacity = "1";
-            tarifs.classList.add("fade");
-        }, 50);
-
-    }, 250);
+    const t = document.getElementById("tarifs");
+    t.style.display = "block";
+    t.classList.add("fade");
 }
 
 function openForm(product) {
-    const form = document.getElementById("formBox");
-    form.style.display = "block";
-    form.classList.add("fade");
+    document.getElementById("formBox").style.display = "block";
     window.selectedProduct = product;
+
+    document.getElementById("productInput").value = product;
 }
-
-function sendMail() {
-    const uid = document.getElementById("uid").value;
-    const ref = document.getElementById("ref").value;
-
-    const subject = "Commande Gaming-z Gems";
-
-    const body =
-        "Produit: " + window.selectedProduct +
-        "%0AUID: " + uid +
-        "%0ARéférence: " + ref;
-
-    window.location.href =
-        "mailto:zunorabj@gmail.com?subject=" + subject + "&body=" + body;
-                                        }
