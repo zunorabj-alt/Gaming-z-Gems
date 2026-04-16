cb92d9107a11ad350393b8ff2862d3afab72ec36
zunorabj-alt <zunorabj@gmail.com>
2026-04-16 15:42:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function showGame() {
     document.querySelector(".games").style.display = "none";
-    document.getElementById("tarifs").style.display = "block";
+
+    const t = document.getElementById("tarifs");
+    t.style.display = "block";
 }
 
 function goBack() {
@@ -11,6 +13,8 @@ function goBack() {
 
 function openForm(product) {
     document.getElementById("formBox").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+
     window.selectedProduct = product;
     document.getElementById("productInput").value = product;
 }
@@ -18,17 +22,4 @@ function openForm(product) {
 function closeForm() {
     document.getElementById("formBox").style.display = "none";
     document.getElementById("overlay").style.display = "none";
-}
-
-/* SUBMIT ANIMATION + SEND */
-function sendOrder(event) {
-    event.preventDefault();
-
-    document.getElementById("successBox").style.display = "flex";
-
-    setTimeout(() => {
-        event.target.submit();
-    }, 1500);
-
-    return false;
 }
