ee6fd47aecf9e61c6d81c7f4c4caf71d5c9f860a
zunorabj-alt <zunorabj@gmail.com>
2026-04-18 19:01:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,90 +1,93 @@
-/* OPEN POPUP */
+let cart=[];
+let qty=1;
+
+/* ADD */
+function addToCart(e,product){
+e.stopPropagation();
+cart.push({product,qty:1});
+updateCart();
+openForm(product);
+}
+
+/* CART */
+function updateCart(){
+document.getElementById("cartCount").innerText=cart.length;
+}
+
+function openCart(){
+document.getElementById("cartBox").style.display="block";
+renderCart();
+}
+
+function closeCart(){
+document.getElementById("cartBox").style.display="none";
+}
+
+function renderCart(){
+let box=document.getElementById("cartItems");
+box.innerHTML="";
+cart.forEach(i=>{
+box.innerHTML+="<p>"+i.product+" x"+i.qty+"</p>";
+});
+}
+
+/* SEARCH */
+function searchItems(){
+let v=document.getElementById("searchBar").value.toLowerCase();
+document.querySelectorAll(".card").forEach(c=>{
+c.style.display=c.innerText.toLowerCase().includes(v)?"block":"none";
+});
+}
+
+/* FORM */
 function openForm(product){
-let popup = document.getElementById("formBox");
-let overlay = document.querySelector(".overlay");
-
-popup.classList.add("show");
-overlay.style.display="block";
-
-document.getElementById("productInput").value = product;
+document.getElementById("formBox").style.display="block";
+document.getElementById("selectedProduct").innerText=product;
+document.getElementById("productInput").value=product;
 }
 
-/* CLOSE POPUP */
-function closeForm(){
-let popup = document.getElementById("formBox");
-let overlay = document.querySelector(".overlay");
-
-popup.classList.remove("show");
-
-setTimeout(()=>{
-overlay.style.display="none";
-},200);
+function changeQty(v){
+qty+=v;
+if(qty<1)qty=1;
+document.getElementById("qty").innerText=qty;
 }
 
-/* SHOW TARIFS */
+/* NAV */
 function showGame(){
 document.querySelector(".games").style.display="none";
 document.getElementById("tarifs").style.display="block";
 }
 
-/* RETOUR */
 function goBack(){
 document.querySelector(".games").style.display="flex";
 document.getElementById("tarifs").style.display="none";
 }
 
-/* THEME */
 function toggleTheme(){
 document.body.classList.toggle("light");
 }
 
-/* ID */
-function generateOrderID(){
-return "GZG-" + Date.now() + "-" + Math.floor(Math.random()*1000);
+/* RECEIPT */
+function downloadReceipt(){
+let text="Commande:\n";
+cart.forEach(i=>text+=i.product+"\n");
+
+let b=new Blob([text],{type:"text/plain"});
+let a=document.createElement("a");
+a.href=URL.createObjectURL(b);
+a.download="recu.txt";
+a.click();
 }
 
-/* SEND ORDER */
+/* ORDER */
 function sendOrder(e){
-
-let form = e.target;
-
-let uid = form.querySelector('input[name="uid"]').value;
-let ref = form.querySelector('input[name="ref"]').value;
-let pseudo = form.querySelector('input[name="pseudo"]').value;
-let product = document.getElementById("productInput").value;
-
-let orderID = generateOrderID();
-
-/* REMOVE OLD */
-form.querySelectorAll(".auto-field").forEach(el => el.remove());
-
-function addField(name,value){
-let input=document.createElement("input");
-input.type="hidden";
-input.name=name;
-input.value=value;
-input.classList.add("auto-field");
-form.appendChild(input);
-}
-
-addField("order_id",orderID);
-addField("uid_copy",uid);
-addField("ref_copy",ref);
-addField("pseudo_copy",pseudo);
-addField("product_copy",product);
-
-/* POPUP */
+e.preventDefault();
 document.getElementById("successBox").style.display="flex";
-document.getElementById("orderMsg").innerHTML =
-"COMMANDE REÇUE 🎮🔥<br><br>ID: <b>"+orderID+"</b><br>"+
-"UID: "+uid+"<br>"+
-"Pseudo: "+pseudo+"<br>"+
-"Référence: "+ref+"<br>"+
-"Produit: "+product;
-
-/* CLOSE FORM */
-document.getElementById("formBox").classList.remove("show");
-document.querySelector(".overlay").style.display="none";
-
-return true;
+document.getElementById("orderMsg").innerText="Commande envoyée";
+return false;
 }
+
+/* HELP */
+function openHelp(){
+alert("Aide bientôt disponible");
+  }
